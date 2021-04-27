385e922d8377a74de1c157e6206b96b0e411d047
oguzhannx <oguzhanoztr00@gmail.com>
2021-04-27 15:39:53 +0300

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DataAccess.Concrete.InMemory;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InMemoryCarDal inMemoryCarDal = new InMemoryCarDal();
+            foreach (var a in inMemoryCarDal.GetAll())
+            {
+                System.Console.WriteLine(a.Description);
+            }
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InMemoryCarDal inMemoryCarDal = new InMemoryCarDal();
+            foreach (var a in inMemoryCarDal.GetAll())
+            {
+                System.Console.WriteLine(a.Description);
+            }
+        }
+    }
+}
